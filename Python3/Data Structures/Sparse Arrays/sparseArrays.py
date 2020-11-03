655db64a16744e1a85ca5c69d7844d9deff1f35c
Nicholas <47710136+nml010@users.noreply.github.com>
2020-11-03 01:26:41 -0600

    Solutions

---
+++ Python3/Data Structures/Sparse Arrays/sparseArrays.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    results = []
+
+    for q in queries:
+        counter = 0        
+        for n in strings:
+            if str(q) == str(n):
+                counter += 1
+        results.append(counter)
+    return (results)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
