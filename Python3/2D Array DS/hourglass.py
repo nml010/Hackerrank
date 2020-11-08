5c8e9003d1e732df44d87e78c605c65e64ad7886
Nicholas <47710136+nml010@users.noreply.github.com>
2020-11-08 16:22:41 -0600

    Create hourglass.py

---
+++ Python3/2D Array DS/hourglass.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    maxSum = -63   
+    
+    for i in range(4):
+        for n in range(4):
+            top = sum(arr[i][n:n+3])
+            mid = arr[i+1][n+1]
+            bottom = sum(arr[i+2][n:n+3])
+            
+            hourglass = top + mid + bottom
+            
+            if (hourglass > maxSum):
+                maxSum = hourglass
+    return maxSum
+                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
